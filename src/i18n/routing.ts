e247bf7af46ac8b67595b5c712bd4c46496a9bf7
Carlos Jose Sanchez <carlosjosesancheze@gmail.com>
2026-03-17 18:30:24 -0400

    routing

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,6 +3,8 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["en", "es"] as const,
   defaultLocale: "en",
+  localeDetection: true,
+  localePrefix: "as-needed",
 });
 
 export type Locale = (typeof routing.locales)[number];
