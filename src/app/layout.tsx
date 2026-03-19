1a1d98d37c07e0075a8e94ed2db32905e8572c72
Carlos Jose Sanchez <carlosjosesancheze@gmail.com>
2026-03-19 17:53:24 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body className={`antialiased`}>{children}</body>
     </html>
   );
